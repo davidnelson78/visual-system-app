223dc4ba236f74651ebebe3f30e6c9667c1e9b02
David Nelson <david.a.nelson78@gmail.com>
2020-09-18 18:26:15 -0500

    master

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from pages import (
     overview,
-    osAntivirus,
+    os_antivirus,
     features,
     firmware,
     modules,
@@ -25,8 +25,8 @@ app.layout = html.Div(
 # Update page
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == "/visual-system-app/osAntivirus":
-        return osAntivirus.create_layout(app)
+    if pathname == "/visual-system-app/os_antivirus":
+        return os_antivirus.create_layout(app)
     elif pathname == "/visual-system-app/features":
         return features.create_layout(app)
     elif pathname == "/visual-system-app/firmware":
@@ -38,7 +38,7 @@ def display_page(pathname):
     elif pathname == "/visual-system-app/full-view":
         return (
             overview.create_layout(app),
-            osAntivirus.create_layout(app),
+            os_antivirus.create_layout(app),
             features.create_layout(app),
             firmware.create_layout(app),
             modules.create_layout(app),
